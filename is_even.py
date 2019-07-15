272f2cc66b51669758e4ce2e748f60229f8639f9
pcrystal <52405714+pcrystal@users.noreply.github.com>
2019-07-15 17:11:41 +0100

    Update is_even.py

--- is_even.py
+++ is_even.py
@@ -1,26 +1,33 @@
 #funtion tools
-#
+
+
 #import functools
 #from functools import reduce
-#
+
 #total = reduce(lambda item,running_total:item + running_total,[1,2,3,4,5])
+
 #print(total)
 
 #     how to determine even numbers
+
 #def is_even(numbers): return numbers % 2== 0
   
 #numbers = [1,56,234,87,4,76,24,69,90,135]
 #
 #print(list(filter(is_even,numbers)))
 
+
 #        how to determine odd numbers
+
 #def is_odd(numbers): return numbers % 2== 1
     
 #numbers = [1,56,234,87,4,76,24,69,90,135]
 
 #print(list(filter(is_odd,numbers)))
-#
+
+
 #   how to determine even numbers using lambda
+
 
 #def is_even(numbers): return numbers % 2== 0
   
@@ -30,8 +37,9 @@
 #
 #print(list(filter(lambda numbers:numbers%2==1,numbers)))
 
-#         spilting words and length of words
-                
+
+#        spilting words and length of words
+
 #sentence = "the quick brown fox jumps over the lazy dog"
 #words = sentence.split()
 #print (words)
@@ -53,7 +61,7 @@
 #print(list(filter(lambda list1:list1 %2==1,list1)))
 #print([u for u in list1 if u%2==0])
 #print([u for u in list1 if u%2==1])
-#  
+  
 #        length of words
 
 #def alphabets():
